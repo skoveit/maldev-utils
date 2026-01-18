fd396505bf75e44add98e791643faf7a1207191b
0xOral <alqudsisours@gmail.com>
2026-01-18 15:03:46 +0200

    process injection

--- MalDives-Framework/include/process_injection.h
+++ MalDives-Framework/include/process_injection.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <windows.h>
 
-BOOL GetRemoteProcessHandle(LPWSTR szProcessName, DWORD* dwProcessId, HANDLE* hProcess);
 BOOL InjectDllToRemoteProcess(HANDLE hProcess, LPWSTR DllName);
 BOOL InjectShellcodeToRemoteProcess(HANDLE hProcess, PBYTE pShellcode, SIZE_T sSizeOfShellcode);
